f2468d015466affb11f13b0fec27a37561bc80ee
ridsb <riddhi.bisht2021@vitstudent.ac.in>
2024-11-15 23:39:08 +0530

    added backend live url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000', // replace with your backend URL if deployed
+  baseURL: 'https://spyne-ai-assessment.onrender.com',
+  ///'http://localhost:5000', 
+  // replace with your backend URL if deployed
 });
 
 // Attach JWT token to each request
